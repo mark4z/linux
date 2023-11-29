cb740e1bc5a7fa0cad3007cc21bd94b8bc56a5a0
mark4z <mark4z@apache.org>
2023-11-29 13:54:43 +0000

    debug

---
+++ init.sh
@@ -0,0 +1,16 @@
+#https://risc-v-getting-started-guide.readthedocs.io/en/latest/linux-qemu.html
+#https://github.com/riscv-collab/riscv-gnu-toolchain
+#https://github.com/mirror/busybox.git
+
+apt update
+apt install -y autoconf automake autotools-dev curl libmpc-dev libmpfr-dev libgmp-dev \
+                 gawk build-essential bison flex texinfo gperf libtool patchutils bc \
+                 zlib1g-dev libexpat-dev git bear libpixman-1-dev
+
+apt-get install -y git build-essential gdb-multiarch qemu-system-misc \
+gcc-riscv64-linux-gnu binutils-riscv64-linux-gnu ninja-build
+
+# qemu-system-riscv64 -nographic -machine virt \
+#      -kernel arch/riscv/boot/Image -append "root=/dev/vda rw console=ttyS0,nokaslr" \
+#      -drive file=./rootfs.img,format=raw,id=hd0 \
+#      -device virtio-blk-device,drive=hd0 -s -S
