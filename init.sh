2b50bd4ea2c4cd170cb270a8dc4fa1d2eb19c4af
mark4z <mark4z@apache.org>
2023-11-29 15:01:02 +0000

    debug

--- init.sh
+++ init.sh
@@ -5,10 +5,12 @@
 apt update
 apt install -y autoconf automake autotools-dev curl libmpc-dev libmpfr-dev libgmp-dev \
                  gawk build-essential bison flex texinfo gperf libtool patchutils bc \
-                 zlib1g-dev libexpat-dev git bear libpixman-1-dev
+                 zlib1g-dev libexpat-dev git bear libpixman-1-dev libncurses-dev
 
 apt-get install -y git build-essential gdb-multiarch qemu-system-misc \
 gcc-riscv64-linux-gnu binutils-riscv64-linux-gnu ninja-build
+
+#make ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- menuconfig
 
 # qemu-system-riscv64 -nographic -machine virt \
 #      -kernel arch/riscv/boot/Image -append "root=/dev/vda rw console=ttyS0,nokaslr" \
